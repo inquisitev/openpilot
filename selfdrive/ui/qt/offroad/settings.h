b80599a20a458f787ddb3a1055ab58d9a7b71959
Trevor Keegan <tkgn15@live.com>
2024-11-22 18:57:48 -0600

    Brightness control and dist btn exp mode
    update brightness
    
    add prints
    
    remove couts
    
    dist btn toggle exp
    
    tune brightness
    
    branch is tested

--- selfdrive/ui/qt/offroad/settings.h
+++ selfdrive/ui/qt/offroad/settings.h
@@ -71,6 +71,9 @@ private:
   Params params;
   std::map<std::string, ParamControl*> toggles;
   ButtonParamControl *long_personality_setting;
+  ButtonParamControl *brightness_setting;
+
+
 
   void updateToggles();
 };
